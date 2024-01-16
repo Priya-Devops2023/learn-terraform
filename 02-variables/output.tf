2d7f127b5f7caaf66778e759de62239018641f2d
Priya <priaysu@gmail.com>
2024-01-16 15:13:13 -0600

    Created the variables files for practice

---
+++ 02-variables/output.tf
@@ -0,0 +1,27 @@
+output "v1" {
+  value = var.v1
+}
+
+output "First_Value_in_List" {
+  value = var.v2[0]
+}
+
+output "Second_Value_in_List" {
+  value = var.v2[1]
+}
+
+output "Value_in_Map" {
+  value = var.v3["abc"]
+}
+
+output "Value_in_Map1" {
+  value = lookup(var.v3, "abc1", "none")
+}
+
+output "Second_Value_in_List1" {
+  value = element(var.v2, 4)
+}
+
+output "FruitDetails" {
+  value = "Fruit Name - Orange, Quantity - ${var.fruits["orange"]["quantity"]}"
+}
---
+++ 02-variables/vars.tf
@@ -0,0 +1,28 @@
+variable "v1" {
+  default = "Hello World"
+}
+
+variable "v2" {
+  default = [ "Hello World", 100, true ]
+}
+## A variable list can have different data types
+
+variable "v3" {
+  default = {
+    abc = 100
+    xyz = "200"
+  }
+}
+
+variable "fruits" {
+  default = {
+    apple = {
+      price = 10
+      quantity = 200
+    }
+    orange = {
+      price = 5
+      quantity = 100
+    }
+  }
+}
